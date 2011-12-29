49fd2160d2a1ad67cf91fc6fccc4318d7f6554bf
Anton de Winter <adewinter@dimagi.com>
2011-12-29 15:31:10 +0200

    Created model, started initial chart view

--- trackit/models.py
+++ trackit/models.py
@@ -1,3 +1,58 @@
+from django.contrib.auth.models import User
 from django.db import models
+import logging
+import os
+import urllib
+from django.core.files.base import File
+import urlparse
 
-# Create your models here.
+logging.getLogger(__name__)
+
+QUANTITY_DESC = "Quantity for this data point, e.g. 5, 11:23pm, \"Happy\" etc"
+UNIT_DESC = "The Unit associated with this Data Point, e.g. lbs, kph, inches, etc"
+DATAPT_CREATED_ON_DESC = "DateTime this Data Point was created on"
+DATAPT_NOTE_DESC = "Additional information to be associated with this data point"
+
+class Baseline(models.Model):
+    quantity = models.CharField(max_length=50, help_text=QUANTITY_DESC)
+    created_on = models.DateTimeField(auto_now_add=True, help_text=DATAPT_CREATED_ON_DESC)
+    description = models.TextField(null=True, blank=True, help_text=DATAPT_NOTE_DESC)
+    unit = models.CharField(max_length=255, blank=True, null=True, help_text=UNIT_DESC)
+
+    def chart(self):
+        return Chart.objects.filter(baseline=self)
+
+    def __unicode__(self):
+        return "Baseline Quantity: %s, Units: %s, Chart %s" % (self.quantity, self.unit, self.chart)
+
+class Chart(models.Model):
+    UNITSPAN = (
+        ('days', 'Days'),
+        ('minutes', 'Minutes'),
+        ('hours', 'Hours'),
+        ('weeks', 'Weeks'),
+        ('months', 'Months'),
+        ('years', 'Years')
+    )
+
+    name = models.CharField(max_length=255, help_text="The name of this Chart")
+    created_on = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now_add=True, auto_now=True)
+    description = models.TextField(blank=True, null=True, help_text="Chart Description")
+    timespan_units = models.CharField(max_length=50, choices=UNITSPAN, default='days')
+    baseline = models.ForeignKey(Baseline)
+    user = models.ForeignKey(User)
+
+    def __unicode__(self):
+        return "Chart: %s, last_updated: %s" % (self.name, self.last_updated)
+
+class DataPoint(models.Model):
+    quantity = models.CharField(max_length=50, help_text=QUANTITY_DESC)
+    created_on = models.DateTimeField(auto_now_add=True, help_text=DATAPT_CREATED_ON_DESC)
+    note = models.TextField(null=True, blank=True, help_text=DATAPT_NOTE_DESC)
+    unit = models.CharField(max_length=255, blank=True, null=True, help_text=UNIT_DESC)
+    chart = models.ForeignKey(Chart)
+
+    def __unicode__(self):
+        return "Data Point for Chart: %s :: Quant=%s, Units=%s" % (self.chart, self.quantity, self.unit)
+
--- trackit/views.py
+++ trackit/views.py
@@ -1,1 +1,42 @@
-# Create your views here.
+from datetime import datetime
+from django.shortcuts import redirect, render_to_response, get_object_or_404
+from django.http import HttpResponseRedirect, HttpResponseForbidden, HttpResponse
+from django.template import RequestContext
+from django.core.management import call_command
+from trackit.models import Chart, DataPoint
+
+def show_charts(request, userid=None, time_start=None, time_end=None):
+    context = {}
+    if not userid:
+        charts = Chart.objects.all()
+    else:
+        charts = Chart.objects.filter(user__id=userid)
+
+    charts_info = []
+    for chart in charts:
+        charts_info.append(get_chart_dict(chart, time_start, time_end))
+
+    context["charts"] = charts_info
+    return render_to_response("trackit/show_charts.html", context, context_instance=RequestContext(request))
+
+def get_chart_dict(chart, time_start=None, time_end=None):
+    """
+    Given a chart and a time_range, returns a convenient dict
+    containing information to make chart rendering easier.
+    """
+    if not time_end:
+        time_end = datetime.now()
+
+    if not time_start:
+        data_points = DataPoint.objects.filter(chart=chart, created_on__lte=time_end)
+    else:
+        data_points = DataPoint.objects.filter(chart=chart, created_on__lte=time_end, created_on__gte=time_start)
+
+    data_points.order_by(['-created_on'])
+    c = {}
+    c["chart"] = chart
+    data = []
+    for pt in data_points:
+        pass
+
+    return None
